4a962f6f9a65f2f01e51f803992234206499b6c1
Ariel André Aio <104506660+ArielAio@users.noreply.github.com>
2023-10-09 14:37:20 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# ESTATEC
-Site organizador de Estágios
-ESTATEC é um site gerenciador de estágios que tem como objetivo facilitar a busca e gestão de estágios para estudantes e empresas. A plataforma permite que os alunos cadastrem seus currículos e busquem por vagas de acordo com suas áreas de interesse e localização, tornando o processo de busca de estágio mais fácil e organizado.
-
-Participantes: [Ariel Aio](https://github.com/ArielAio), [Arthur Risso](https://github.com/arthurrpr), Cinthia Zague, Gabriel Aguera.
+# estatec
---
+++ view/cadastrar.php
@@ -0,0 +1,83 @@
+<?php
+
+include ('../src/conexao.php');
+
+$buscar_cadastro = 'SELECT * FROM usuarios';
+$query_cadastros = mysqli_query($conn, $buscar_cadastro);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" conteant="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/cadastrar.css">
+  <link rel="shortcut icon" href="assets/images/Logo-03.1.ico" type="image/x-icon">
+  <title>Cadastro ESTATEC</title>
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="form-image">
+      <img src="../view/assets/images/img-cadastrar.svg" alt="Imagem Cadastrar">
+    </div>
+    <div class="form">
+
+      <form action="../src/cadastro.php" method="post">
+        <div class="form-header">
+          <div class="title">
+            <h1>Cadastre-se</h1>
+          </div>
+          <div class="login-button">
+            <button><a href="login.php">Entrar</a></button>
+          </div>
+        </div>
+
+        <div class="input-group">
+            <div class="input-box">
+                <label for="nome">Primeiro Nome</label>
+                <input id="nome" type="text" name="nome" placeholder="Digite seu primeiro nome" required>
+            </div>
+
+            <div class="input-box">
+                <label for="sobrenome">Sobrenome</label>
+                <input id="sobrenome" type="text" name="sobrenome" placeholder="Digite seu sobrenome" required>
+            </div>
+
+            <div class="input-box">
+                <label for="email">Email</label>
+                <input id="email" type="email" name="email" placeholder="Digite seu email" required>
+            </div>
+
+            <div class="input-box">
+                <label for="number">RM</label>
+                <input id="rm" type="text" name="rm" placeholder="Digite seu RM" required>
+            </div>
+
+            <div class="input-box">
+    <label for="senha">Senha</label>
+    <input id="senha" type="password" name="senha" placeholder="Digite sua senha" required>
+    <p id="alerta-senha"></p>
+</div>
+
+<div class="input-box">
+    <label for="senha_confirmacao">Confirme sua senha</label>
+    <input id="senha_confirmacao" type="password" name="senha_confirmacao" placeholder="Confirme sua senha" required>
+</div>
+        </div>
+
+        <input id="cadastrar" class="cadastrar" type="submit" value="Cadastrar" onclick="return validarSenha();">
+      </form>
+
+    </div>
+  </div>
+
+  <script src="assets/js/cadastrar.js"></script>
+
+</body>
+
+</html>
